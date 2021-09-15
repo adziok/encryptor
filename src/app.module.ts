e6650f804584cbc7a363107c93f2a7507014e33e
Adrian Karwowski <adrian.karwowski@elpassion.pl>
2021-09-15 21:47:58 +0200

    feat: add async register to authorization module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { AuthorizationModule } from './authorization/infrastructure/authorization.module';
 
 @Module({
-    imports: [],
+    imports: [
+        AuthorizationModule.registerAsync({
+            useFactory: () => ({
+                hashingSaltRounds: 10,
+                secret: 'secret',
+                signOptions: {
+                    expiresIn: '300s',
+                },
+            }),
+        }),
+    ],
     controllers: [],
     providers: [],
 })
--- src/authorization/infrastructure/authorization.module.ts
+++ src/authorization/infrastructure/authorization.module.ts
@@ -1,4 +1,6 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
 import { AuthorizationFacade } from './authorization.facade';
 import {
     AUTHORIZATION_REPOSITORY_TOKEN,
@@ -7,34 +9,74 @@ import {
 import { InMemoryAuthorizationRepository } from './persistance/in-memory-authorization.repository';
 import { AuthorizationService } from '../application/authorization.service';
 import {
-    IPasswordService,
     PASSWORD_SERVICE_TOKEN,
+    IPasswordService,
 } from '../application/interfaces/password-service.interface';
 import { BcryptPasswordService } from './services/bcrypt-password.service';
+import { AuthorizationConfigService } from './services/authorization-config.service';
+import { JwtService } from './services/jwt.service';
 
-@Module({
-    providers: [
-        AuthorizationFacade,
-        {
-            provide: AUTHORIZATION_REPOSITORY_TOKEN,
-            useClass: InMemoryAuthorizationRepository,
-        },
-        {
-            provide: PASSWORD_SERVICE_TOKEN,
-            useClass: BcryptPasswordService,
-        },
-        {
-            provide: AuthorizationService,
-            useFactory: (
-                passwordService: IPasswordService,
-                authorizationRepository: IAuthorizationRepository,
-            ) =>
-                new AuthorizationService(
-                    passwordService,
-                    authorizationRepository,
-                ),
-            inject: [PASSWORD_SERVICE_TOKEN, AUTHORIZATION_REPOSITORY_TOKEN],
-        },
-    ],
-})
-export class AuthorizationModule {}
+export interface IAuthorizationOptions extends JwtModuleOptions {
+    hashingSaltRounds: number;
+}
+
+export type AuthModuleRegisterFactory = (
+    ...args: any[]
+) => IAuthorizationOptions;
+
+@Module({})
+export class AuthorizationModule {
+    static registerAsync(opts: {
+        useFactory: AuthModuleRegisterFactory;
+        inject?: any[];
+    }): DynamicModule {
+        return {
+            module: AuthorizationModule,
+            imports: [
+                PassportModule,
+                JwtModule.registerAsync({
+                    useFactory: (authorizationConfig: IAuthorizationOptions) =>
+                        opts.useFactory(authorizationConfig),
+                    inject: opts.inject,
+                }),
+            ],
+            providers: [
+                AuthorizationFacade,
+                {
+                    provide: AUTHORIZATION_REPOSITORY_TOKEN,
+                    useClass: InMemoryAuthorizationRepository,
+                },
+                {
+                    provide: PASSWORD_SERVICE_TOKEN,
+                    useClass: BcryptPasswordService,
+                },
+                {
+                    provide: AuthorizationService,
+                    useFactory: (
+                        passwordService: IPasswordService,
+                        authorizationRepository: IAuthorizationRepository,
+                    ) =>
+                        new AuthorizationService(
+                            passwordService,
+                            authorizationRepository,
+                        ),
+                    inject: [
+                        PASSWORD_SERVICE_TOKEN,
+                        AUTHORIZATION_REPOSITORY_TOKEN,
+                    ],
+                },
+                {
+                    provide: AuthorizationConfigService,
+                    useFactory: (authorizationConfig: IAuthorizationOptions) =>
+                        new AuthorizationConfigService(
+                            opts.useFactory(
+                                authorizationConfig,
+                            ).hashingSaltRounds,
+                        ),
+                    inject: opts.inject,
+                },
+                JwtService,
+            ],
+        };
+    }
+}
