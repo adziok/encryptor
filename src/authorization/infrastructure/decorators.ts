e881d9c7c029b739fdf6a48526de6e2ef7c97a5d
Adrian Karwowski <adrian.karwowski@elpassion.pl>
2021-09-18 17:42:49 +0200

    feat: add current user decorator

--- src/authorization/infrastructure/decorators.ts
+++ src/authorization/infrastructure/decorators.ts
@@ -1,4 +1,17 @@
-import { UseGuards } from '@nestjs/common';
+import {
+    createParamDecorator,
+    ExecutionContext,
+    UseGuards,
+} from '@nestjs/common';
 import { HttpJwtStrategy } from './strategies/http-jwt.strategy';
 
 export const Authenticated = () => UseGuards(HttpJwtStrategy);
+
+export type ICurrentUser = {
+    id: string;
+};
+export const CurrentUser = createParamDecorator(
+    (data: unknown, context: ExecutionContext): ICurrentUser => {
+        return context.switchToHttp().getRequest().user;
+    },
+);
