c5c9bfd4e398c91b3828eac7b6acf7858ad1272e
Adrian Karwowski <adrian.karwowski@elpassion.pl>
2021-09-19 11:48:16 +0200

    feat: add nest axios package for rest request purpose

--- src/crypto/presentation/crypto-presentation.module.ts
+++ src/crypto/presentation/crypto-presentation.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { CryptoController } from './controllers/crypto.controller';
 import { CryptoModule } from '../infrastructure/crypto.module';
 
 @Module({
-    imports: [CryptoModule],
+    imports: [CryptoModule, HttpModule],
     controllers: [CryptoController],
 })
 export class CryptoPresentationModule {}
